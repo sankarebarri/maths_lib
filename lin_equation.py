3ee68f4bbe5271bb730f68953e6ddfdd3cff610b
sankarebarri <sankarebarri_dev@yahoo.com>
2024-01-07 17:34:36 +0100

    linear_equation

--- lin_equation.py
+++ lin_equation.py
@@ -4,7 +4,7 @@ def factor_equation(equation):
 def collect_like_terms(equation):
     ...
 
-def linear_equation(equation):
+def linear_equation(equation, n=2):
     if "+" in equation:
         sign = "+"
     else:
@@ -12,11 +12,12 @@ def linear_equation(equation):
     split_equation = equation.split(sign)
     variable = split_equation[0].strip()
     constant = split_equation[1].strip()
-    # print(split_equation)
-    # print(variable, constant)
     if sign == "+":
         constant = -1 * float(constant)
-    # print(f"{variable} = {constant}")
     x = float(constant) / float(variable[0])
-    return x
-# print(linear_equation("3x+2"))
+    if n:
+        return round(x, n)
+    return round(x, 2)
+
+# print(linear_equation("3x+26"))
+# print(linear_equation("3x+26", n=4))
