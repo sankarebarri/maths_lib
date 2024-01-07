dc98b805a0fe550f175cfec6c823b6fe42580e76
sankarebarri <sankarebarri_dev@yahoo.com>
2024-01-07 17:33:45 +0100

    quadratic

--- quadratic.py
+++ quadratic.py
@@ -6,10 +6,8 @@ from lin_equation import linear_equation
 def quadratic_equation(a, b, c, show_step=False):
     determinant = b*b - 4*a*c
     if math.sqrt(determinant).is_integer():
-        a_c = abs(a) * abs(c)
-        # print(a_c) # multiplication of the 'a' term by the 'c' term
+        a_c = abs(a) * abs(c) # multiplication of the 'a' term by the 'c' term
         factor_ac = factorisation(a_c) # a list of pairs of factors of a_c.
-        # print(factor_ac)
         # look for the pairs in 'factor_ac list that would equals the b term
         for factor in factor_ac:
             if diff_sum(factor[0], factor[1], b, a_c) is not None:
@@ -18,21 +16,23 @@ def quadratic_equation(a, b, c, show_step=False):
         if show_step:
             print(f"{a}x^2 + {b_1}x + {b_2}x + {c}")
         cf_1 = hcf(a, b_1) # common factor between 'a' in the equation and b_1
-        # no need of this condition
-        # if cf_1 != 1:
         cf_2 = hcf(b_2, c)
         if show_step:
             print(f"{cf_1}x({a/cf_1}x + {b_1/cf_1}) + {cf_2}({b_2/cf_2}x + {c/cf_2})")
             print(f"({cf_1}x + {cf_2})({b_2/cf_2}x + {c/cf_2})")
         equation_1 = f"{cf_1}x + {cf_2}"
         equation_2 = f"{b_2/cf_2}x + {c/cf_2}"
-        # print(equation_1)
-        # print(equation_2)
         x_1 = linear_equation(equation_1)
         x_2 = linear_equation(equation_2)
-        return x_1, x_2
+        return min(x_1, x_2), max(x_1, x_2)
     else:
         return "Can not be factored"
+
+def quadratic_formula():
+    ...
+
+def completing_the_square():
+    ...
 
 
 
